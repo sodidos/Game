5c0d2cc2463aa1d24251976089bb0b8656d45722
Rémi Sarrailh <remi@madnerd.org>
2024-01-27 16:54:21 +0100

    Modificaion in pathfinding

--- fablab/Scenes/npc/npc.gd
+++ fablab/Scenes/npc/npc.gd
@@ -10,6 +10,8 @@ extends CharacterBody2D
 @onready var interactionTimer := $interactionTimer as Timer
 @onready var Help := $Help as Sprite2D
 
+var moving = true
+
 var rand=RandomNumberGenerator.new()
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,9 +19,10 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	var dir = to_local(nav_agent.get_next_path_position()).normalized()
-	velocity = dir * speed 
-	move_and_slide()
+	if(moving):
+		var dir = to_local(nav_agent.get_next_path_position()).normalized()
+		velocity = dir * speed 
+		move_and_slide()
 	
 func makepath() -> void:
 	nav_agent.target_position = machine.global_position 
@@ -28,14 +31,15 @@ func _on_timer_timeout():
 	makepath()
 
 func _on_navigation_agent_2d_navigation_finished():
+	moving = false
 	pathfinderTimer.stop()
 	var idletimer_rand = rand.randi_range(min_idle, max_idle)
 	print(idletimer_rand)
 	idleTimer.wait_time = idletimer_rand
 	idleTimer.start()
-	
+
 func _on_idle_timer_timeout():
 	print("Help needed")
 	Help.visible = true
-	
+
 
